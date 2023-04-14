154e591aecb222c0235bf44522715890318e4d42
 vladisa88 <vladisa88@gmail.com>
2023-04-14 16:37:29 +0300

    add dir

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import time
+import ray
+from ray.job_submission import JobSubmissionClient
+
+from job1.main import Test
+
+# ray.init(address='51.250.31.207:6379', _node_ip_address='172.18.0.13')
+ray.init(runtime_env={"working_dir": "https://github.com/vladisa88/playground/archive/refs/heads/master.zip",})
+
+def _run_job(match_id: int, client_id: int) -> None:
+    print("Try to run job")
+    client = JobSubmissionClient("http://localhost:8285")
+    print("Submitting job")
+    job_id = client.submit_job(
+        entrypoint="python job1/main.py",
+        runtime_env={
+            "working_dir": "https://github.com/vladisa88/playground/archive/refs/heads/master.zip",
+            "env_vars": {"MATCH_ID": str(match_id), "CLIENT_ID": str(client_id)}
+        },
+    )
+    print("Job ID: ", job_id)
+
+
+def _run_actor() -> None:
+    ref = Test.remote()
+    ref.main.remote()
+
+
+def main() -> None:
+    print("HERERERERE!!!!!")
+    while True:
+        for i in range(3):
+            _run_job(i, i + 1)
+        time.sleep(100)
+
+if __name__ == "__main__":
+    main()
