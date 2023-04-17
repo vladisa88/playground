9cf5a6f3e3d5f3e569adef5bd4c15bb3c4b2717e
 vladisa88 <vladisa88@gmail.com>
2023-04-17 09:26:51 +0300

    fix: update main py

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ def _run_job(match_id: int, client_id: int) -> None:
     client = JobSubmissionClient("http://localhost:8285")
     print("Submitting job")
     job_id = client.submit_job(
-        entrypoint="curl -LJO https://github.com/vladisa88/playground/archive/refs/heads/master.zip -o /tmp/master.zip && unzip -q /tmp/master.zip && cd playground-master && python src/job1/main.py",
+        entrypoint="curl https://github.com/vladisa88/playground/archive/refs/heads/master.zip -LJON -s && unzip -q playground-master.zip && ls -a && cd playground-master && ls -a && python src/job1/main.py",
         runtime_env={
             "working_dir": ".",
             "env_vars": {"MATCH_ID": str(match_id), "CLIENT_ID": str(client_id)}
