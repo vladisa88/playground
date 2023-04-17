bd010f821a3fef844bb2a4048b73a291b6ade099
 vladisa88 <vladisa88@gmail.com>
2023-04-17 09:39:02 +0300

    fix2

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ from ray.job_submission import JobSubmissionClient
 from job1.main import Test
 
 # ray.init(address='51.250.31.207:6379', _node_ip_address='172.18.0.13')
-ray.init(runtime_env={"working_dir": "https://github.com/vladisa88/playground/archive/refs/heads/master.zip",})
+ray.init()
 
 def _run_job(match_id: int, client_id: int) -> None:
     print("Try to run job")
     client = JobSubmissionClient("http://localhost:8285")
     print("Submitting job")
     job_id = client.submit_job(
-        entrypoint="curl -LJON -s https://github.com/vladisa88/playground/archive/refs/heads/master.zip && ls -a && unzip -q playground-master.zip && ls -a && cd playground-master && ls -a && python src/job1/main.py",
+        entrypoint="ls -a && curl -LJON -s https://github.com/vladisa88/playground/archive/refs/heads/master.zip && ls -a && unzip -q playground-master.zip && ls -a && cd playground-master && ls -a && python src/job1/main.py",
         runtime_env={
             "working_dir": ".",
             "env_vars": {"MATCH_ID": str(match_id), "CLIENT_ID": str(client_id)}
